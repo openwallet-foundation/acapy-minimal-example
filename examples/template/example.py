a05ead58c9d03ed098c15a560f6779b4b829f0d4
Athan Massouras <athan@indicio.tech>
2025-08-06 16:15:30 +0300

    fix: lint
    Signed-off-by: Athan Massouras <athan@indicio.tech>

--- examples/template/example.py
+++ examples/template/example.py
@@ -16,12 +16,14 @@ BOB = getenv("BOB", "http://bob:3001")
 
 LOGGER = logging.getLogger(__name__)
 
+
 async def main():
     """Test Controller protocols."""
     async with Controller(base_url=ALICE) as alice, Controller(base_url=BOB) as bob:
         await didexchange(alice, bob)
 
     LOGGER.debug("Done")
+
 
 if __name__ == "__main__":
     logging_to_stdout()
